bb490908e2463f2067cb08d9baaeb0040aa74d2e
Phillip Curry <step3software@gmail.com>
2023-01-04 13:39:10 -0500

    Resume

---
+++ src/views/Resume.vue
@@ -0,0 +1,288 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <div class="name">Phillip Curry</div>
+      <div class="address">1136 Mason Woods Dr | Atlanta, GA 30329</div>
+      <div class="contact">(404) 825 6231 | step3software@gmail.com</div>
+    </div>
+    <h1>Employment</h1>
+    <div class="section">
+      <div class="headline">
+        <div class="company">Founder at Step 3 Software</div>
+        <div class="time">September 2020 – present</div>
+      </div>
+      <div class="indent">
+        <div>Individually responsible for the full product cycle with clients</div>
+      </div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <div class="company">Virtual Behavioral Skills Training</div>
+      </div>
+      <div class="indent">
+        Working with some researchers at St. Cloud U. to
+        design and develop an online skills training application for teachers of students
+        with behavioral challenges`
+        <div class="technologies">Vue.js, Babel, nodejs</div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="headline">
+        <div class="company">Technical Architect at Jonah Group (now 3Pillar Global)</div>
+        <div class="time">August 2022 - present</div>
+      </div>
+      <div class="indent">Tech lead/project manager on a 2m+ client account with significant responsibilities in terms
+        of revenue growth, hiring, as well as solution design
+      </div>
+    </div>
+    <div class="section">
+      <div class="headline">
+        <div class="company">Technical Developer at Jonah Group</div>
+        <div class="time">July 2012 - August 2022</div>
+      </div>
+      <div class="indent">Individual contributor with increasing experience on various client projects</div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <div class="company">Tech Lead at client Farm Credit Canada</div>
+        <div class="time">July 2021 – present</div>
+      </div>
+      <div class="indent">
+        <div class="project pad-x-1">Agile scrum teams converting an existing “monolith” Java 8 ULC application to a
+          modern,
+          RESTful architecture leveraging AWS infrastructure with Angular 2 clients
+          Developed full-stack, both front-end and back-end as needed.
+        </div>
+        <div class="responsibility">Analyzed existing codebase and application to write requirements and recommend
+          enhancements
+        </div>
+        <div class="responsibility">Designed and implemented full-stack applications in a microservice + webapp
+          architecture
+        </div>
+        <div class="responsibility">Developed and maintained extensive IT and unit tests</div>
+        <div class="responsibility">Upgraded multiple projects from Angular 5+ to Angular 12</div>
+        <div class="responsibility">Responsible for code reviews, branch management, deployments, production `releases
+        </div>
+        <div class="responsibility">Facilitated refinements, stand-ups, retrospectives</div>
+        <div class="responsibility">Led the onboarding effort going from 1 team of 3 developers to 2 teams, 10
+          developers, a senior analyst and a scrum-master
+        </div>
+        <div class="responsibility">Mentored a new Tech Lead, as well as several Developers</div>
+        <div class="technologies">Angular 14, TypeScript, Java 12, Spring, AWS, PostgreSQL</div>
+      </div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <div class="company">Senior Developer at client Ontario Teachers Pension Plan</div>
+        <div class="time">September 2015 – March 2020</div>
+      </div>
+      <div class="indent">
+        <div class="project">The project converted various legacy SmallTalk application to modern browser apps
+          and web services. The frontend is written with Angular.js, the backend is Java 8 with
+          Spring and Hibernate.
+        </div>
+        <div class="responsibility">Analyzed existing codebase and application to write requirements as well as
+          recommending enhancements
+        </div>
+        <div class="responsibility">Designed and implemented UI applications using FLUX and MVC patterns</div>
+        <div class="responsibility">Developed and maintained extensive E2E, IT and unit tests</div>
+        <div class="responsibility">Responsible for code reviews, branch management, deployments</div>
+        <div class="technologies">Angular.js, TypeScript, Java 8, Spring, Hibernate, DB2, Gulp, TestNG, Jasmine, Bamboo,
+          Git
+        </div>
+      </div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <div class="company">Developer at client Blue Care Network</div>
+        <div class="time">July 2014 – March 2015</div>
+      </div>
+      <div class="indent">
+        <div class="project">The Medical Informatics project processes large quantities of client data from
+          multiple sources to generate reports.
+        </div>
+        <div class="responsibility">Analyzed database scripts in order to recommend performance enhancements</div>
+        <div class="technologies">Oracle SQL, Harvest SCM</div>
+      </div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <div class="company">Developer at client Bank of Montreal</div>
+        <div class="time">April 2014 – June 2014</div>
+      </div>
+      <div class="indent">
+        <div class="project">The valuation product control (VPC) adjustments application is used by Bank of
+          Montreal to book valuation adjustments for trade portfolios. It consists of a Microsoft Excel plug-in to
+          collect data and web interface to view data and generate reports.
+        </div>
+        <div class="responsibility">Worked closely with stakeholders to design and rapidly implement a functional
+          prototype
+        </div>
+        <div class="technologies"> C#, .NET, Microsoft Excel, ReSharper, IIS, MSBuild, SQL Server, SVN</div>
+      </div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <div class="company">Developer at client Bank of Montreal</div>
+        <div class="time">January 2014 – April 2014</div>
+      </div>
+      <div class="indent">
+        <div class="project">The User Rights application allows Bank of Montreal to manage permissions from an
+          internal browser-based application.
+        </div>
+        <div class="responsibility">Worked closely with framework developers to develop a dashboard within their
+          ecosystem
+        </div>
+        <div class="responsibility">Designed and implemented a UI applications</div>
+        <div class="responsibility">Developed and maintained IT and unit tests</div>
+        <div class="responsibility">Responsible for code reviews, branch management, deployments</div>
+        <div class="technologies">C#, .NET, Silverlight, Telerik, ReSharper, IIS, MSBuild, SQL Server, SVN</div>
+      </div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <div class="company">Junior Developer at client Blue Cross Blue Shield of Michigan</div>
+        <div class="time">July 2012 – December 2013</div>
+      </div>
+      <div class="indent">
+        <div class="project">The Benefit Configurator system is a strategic, complex component of BCBSM’s
+          Michigan Operating System (MOS). The system allows for the detailed and efficient
+          configuration and maintenance of a wide range of benefit packages, and to have
+          those benefits implemented automatically across the enterprise.
+        </div>
+        <div class="responsibility">Developed JavaScript components from scratch</div>
+        <div class="responsibility">Analyzed existing code base to reproduce like-for-like browser application</div>
+        <div class="responsibility">Responsible for code reviews, branch management, deployments</div>
+        <div class="technologies">Java 6, J2EE, Spring, JavaScript, JSP, VB6, Oracle SQL, SVN</div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="headline">
+        <div class="company">Junior Software Developer at Diamond Computing Company</div>
+        <div class="time">2012</div>
+      </div>
+      <div class="indent">
+        <div class="responsibility">implemented simple solution in Visual Basic 2005</div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="headline">
+        <div class="company">Assisstant Lecturer at Hong Kong University S.P.A.C.E. Global College</div>
+        <div class="time">2009 – 2011</div>
+      </div>
+      <div class="indent">
+        <div class="responsibility">Acquired skills in leadership and inter-cultural communication</div>
+      </div>
+    </div>
+    <h1>Education & Certification</h1>
+    <div>Oracle Certified Professional, Java SE 6 Programmer</div>
+    <div><span class="bold">Georgia State University</span>, Atlanta, USA</div>
+    <div class="indent">Course work in Computer Science</div>
+    <div><span class="bold">Simon Frasier University</span>, Vancouver, Canada</div>
+    <div class="indent">Course work in Computer Science</div>
+    <div><span class="bold">McGill University</span>, Montreal, Canada</div>
+    <div class="indent">B.A. in Linguistics, Classics</div>
+    <h1>Personal</h1>
+    <div>Dual-citizen of USA and Canada</div>
+    <div>Fluent in English, German, Conversational in Chinese, French, Spanish</div>
+    <div>Summer 2015: Hiked the Appalachian Trail</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ResumePage"
+};
+</script>
+<style scoped>
+a {
+  padding-right: 4px;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+h1 {
+  padding-left: 1rem;
+}
+
+.container {
+  text-align: left;
+}
+
+.header {
+  display: grid;
+  grid-template-areas: "a b" "a c";
+}
+
+.name {
+  grid-area: a;
+  font-size: 2.2rem;
+  text-align: left;
+}
+
+.address {
+  grid-area: b;
+}
+
+.contact {
+  grid-area: c;
+}
+
+.headline {
+  font-size: 1.2rem;
+}
+
+.time {
+}
+
+.company {
+  font-weight: bolder;
+}
+
+.role {
+  font-weight: bold;
+  font-style: italic;
+}
+
+.location {
+}
+
+.section {
+  padding-bottom: 1rem;
+}
+
+.client {
+}
+
+.project {
+  padding-bottom: .2rem;
+}
+
+.responsibility {
+  display: list-item;
+  margin-left: .8rem;
+}
+
+.technologies {
+  font-style: italic;
+}
+
+.header {
+  text-align: right;
+}
+
+.indent {
+  padding-left: 1rem;
+}
+
+.bold {
+  font-weight: bold;
+}
+
+.pad-x-1 {
+  padding-bottom: .5rem;
+}
+
+.pad-x-2 {
+  padding-bottom: 1rem;
+}
+</style>
