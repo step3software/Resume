a3a14b979015701c6d413a94fb9087e6b6ee70ad
phillip <step3software@gmail.com>
2023-07-07 21:23:06 -0400

    1 page version of Resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -5,7 +5,8 @@
       <div class="address">1136 Mason Woods Dr | Atlanta, GA 30329</div>
       <div class="contact">(404) 825 6231 | step3software@gmail.com</div>
     </div>
-    <h1>Skills</h1>
+    <h1>Core Skills</h1>
+    <div><span class="category">Project Management: </span>Agile, Scrum, Certified SAFe® 6 Practitioner, Analysis, Estimation, KPIs</div>
     <div><span class="category">Languages: </span> Java, JavaScript, TypeScript, C#</div>
     <div><span class="category">Frameworks: </span> Angular.js, Vue.js, Spring (MVC, Boot, Batch, Test)</div>
     <div><span class="category">Database: </span> PostgreSQL, Oracle SQL, Liquibase, ElasticSearch</div>
@@ -13,173 +14,94 @@
     <h1>Employment</h1>
     <div class="section">
       <div class="headline">
-        <div class="company">Technical Architect at Jonah Group (now 3Pillar Global)</div>
-        <div class="time">August 2022 - present</div>
+        <span class="company">Founder at Step 3 Software</span>
+        <span class="time">July 2020 – present</span>
       </div>
-      <div class="indent">Tech lead on a 2m+ client account with significant responsibilities in terms
-        of revenue growth, hiring, as well as solution design
-        <div class="responsibility">Lead Developer on a pioneer team of four</div>
-        <div class="responsibility">Onboarded thirteen developers and various other roles from Jonah at FCC</div>
-        <div class="responsibility">Mentored several developers and dev leads</div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <span class="company">Client: Virtual Behavioral Skills Training</span>
+        <span class="time">July 2020 – present</span>
+      </div>
+      <div class="indent">
+        <div class="technologies">Vue.js, Babel, nodejs, SQLite</div>
+        <div class="responsibility">Solely responsible for entire SDLC</div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="headline">
+        <span class="company">Technical Architect at Jonah Group (now 3Pillar Global)</span>
+        <span class="time">July 2012 - March 2020, July 2021 - present</span>
+      </div>
+    </div>
+    <div class="client section">
+      <div class="headline">
+        <span class="company">Lead Developer at Client: Farm Credit Canada</span>
+        <span class="time">July 2021 – present</span>
+      </div>
+      <div class="indent">
+        <div class="technologies">SAFe, Angular 14, TypeScript, Tailwind, Java 17, Spring Boot and Batch, AWS, PostgreSQL, Liquibase</div>
+        <div class="responsibility">Lead a pioneer team of four which grew to a 2m+ account over 2 years</div>
+        <div class="responsibility">Onboarded and mentored 10+ developers and 3 dev leads </div>
         <div class="responsibility">Interviewed and evaluated developers of all levels</div>
-        <div class="responsibility">Approved and reconciled timesheets for the account</div>
+        <div class="responsibility">Introduced time-sheet reconciliation process to get invoice error rate to near 0</div>
       </div>
     </div>
     <div class="client section">
       <div class="headline">
-        <div class="company">Client: Farm Credit Canada</div>
-        <div class="time">July 2021 – present</div>
+        <span class="company">Senior Developer at Client: Ontario Teachers Pension Plan</span>
+        <span class="time">September 2015 – March 2020</span>
       </div>
       <div class="indent">
-        <div class="project pad-x-1">Geographically distributed Agile scrum teams converting an existing “monolith” Java 8 ULC application to a
-          modern microservice architecture with Angular.js webapps, leveraging AWS
-        </div>
-        <div class="technologies">Angular 14, TypeScript, Tailwind, Java 17, Spring Boot and Batch, AWS, PostgreSQL, Liquibase, Git</div>
-        <div class="responsibility">Analyzed existing codebase and application to write requirements and recommend
-          enhancements, provide estimates
-        </div>
-        <div class="responsibility">Designed and implemented full-stack applications with a microservice + webapp
-          environment including extensive IT and unit tests</div>
-        <div class="responsibility">Upgraded eight projects from Angular 5+ to Angular 17, Java 8 to 17</div>
-        <div class="responsibility">Responsible for code reviews, branch management, deployments, production releases
-        </div>
-        <div class="responsibility">Facilitated refinements, stand-ups, retrospectives</div>
-        <div class="responsibility">Oversaw teams through organizational adoption of SAFe</div>
-      </div>
-    </div>
-    <div class="section">
-      <div class="headline">
-        <div class="company">Founder at Step 3 Software</div>
-        <div class="time">September 2020 – present</div>
-      </div>
-      <div class="indent">
-        <div>Individually responsible for the full product cycle with clients</div>
-      </div>
-    </div>
-    <div class="client section">
-      <div class="headline">
-        <div class="company">Client: Virtual Behavioral Skills Training</div>
-      </div>
-      <div class="indent">
-        Working with some researchers at St. Cloud U. to
-        design and develop an online skills training application for teachers of students
-        with behavioral challenges`
-        <div class="technologies">Vue.js, Babel, nodejs, SQLite, Git</div>
-      </div>
-    </div>
-    <div class="section" style="padding-top:2.2rem;">
-      <div class="headline">
-        <div class="company">Technical Developer at Jonah Group</div>
-        <div class="time">July 2012 - March 2020, July 2021 - August 2022</div>
-      </div>
-      <div class="indent">Individual contributor with progressive responsibilities on various client projects</div>
-    </div>
-    <div class="client section">
-      <div class="headline">
-        <div class="company">Client: Ontario Teachers Pension Plan</div>
-        <div class="time">September 2015 – March 2020</div>
-      </div>
-      <div class="indent">
-        <div class="project">This initiative converted a legacy, monolith SmallTalk application to modern browser apps
-          and web services.
-        </div>
         <div class="technologies">Angular.js, TypeScript, Java 8, Spring, Hibernate, DB2, Gulp, TestNG, Jasmine, Bamboo, Git</div>
-        <div class="responsibility">Analyzed existing codebase and application to write requirements as well as
-          recommending enhancements
-        </div>
-        <div class="responsibility">Designed and implemented UI applications using FLUX and MVC patterns, including extensive E2E, IT and unit tests</div>
-        <div class="responsibility">Responsible for code reviews, branch management, deployments</div>
       </div>
     </div>
     <div class="client section">
       <div class="headline">
-        <div class="company">Client: Blue Care Network</div>
-        <div class="time">July 2014 – March 2015</div>
+        <span class="company">Intermediate Developer at Client: Blue Care Network</span>
+        <span class="time">July 2014 – March 2015</span>
       </div>
       <div class="indent">
-        <div class="project">Medical Informatics processed large quantities of client data from
-          multiple sources to generate reports
-        </div>
         <div class="technologies">Oracle SQL, Harvest SCM</div>
-        <div class="responsibility">Analyzed database scripts in order to recommend performance enhancements</div>
       </div>
     </div>
     <div class="client section">
       <div class="headline">
-        <div class="company">Client: Bank of Montreal</div>
-        <div class="time">April 2014 – June 2014</div>
+        <span class="company">Intermediate Developer Client: Bank of Montreal</span>
+        <span class="time">January 2014 – June 2014</span>
       </div>
       <div class="indent">
-        <div class="project">The valuation product control (VPC) adjustments application is used by Bank of
-          Montreal to book valuation adjustments for trade portfolios. It consists of a Microsoft Excel plug-in to
-          collect data and web interface to view data and generate reports.
-        </div>
-        <div class="technologies">C#, .NET, Microsoft Excel, ReSharper, IIS, MSBuild, SQL Server, SVN</div>
-        <div class="responsibility">Worked closely with stakeholders to design and rapidly implement a functional
-          prototype
-        </div>
-      </div>
-    </div>
-    <div class="client section">
-      <div class="headline">
-        <div class="company">Client: Bank of Montreal</div>
-        <div class="time">January 2014 – April 2014</div>
-      </div>
-      <div class="indent">
-        <div class="project">The User Rights application allows Bank of Montreal to manage permissions from an
-          internal browser-based application.
-        </div>
         <div class="technologies">C#, .NET, Silverlight, Telerik, ReSharper, IIS, MSBuild, SQL Server, SVN</div>
-        <div class="responsibility">Worked closely with framework developers to develop a dashboard within their
-          ecosystem
-        </div>
-        <div class="responsibility">Designed and implemented a UI applications, including IT and unit tests</div>
-        <div class="responsibility">Responsible for code reviews, branch management</div>
       </div>
     </div>
     <div class="client section">
       <div class="headline">
-        <div class="company">Client: Blue Cross Blue Shield of Michigan</div>
-        <div class="time">July 2012 – December 2013</div>
+        <span class="company">Junior Developer at Client: Blue Cross Blue Shield of Michigan</span>
+        <span class="time">July 2012 – December 2013</span>
       </div>
       <div class="indent">
-        <div class="project">Benefit Configurator system was a strategic, complex component of BCBSM’s
-          OS
-        </div>
         <div class="technologies">Java 6, J2EE, Spring, JavaScript, JSP, VB6, Oracle SQL, SVN</div>
-        <div class="responsibility">Analyzed existing code base to reproduce like-for-like browser application</div>
-        <div class="responsibility">Responsible for branch management</div>
-      </div>
-    </div>
-    <div class="section" style="padding-top:1rem">
-      <div class="headline">
-        <div class="company">Junior Software Developer at Diamond Computing Company</div>
-        <div class="time">2012</div>
-      </div>
-      <div class="indent">
-        <div class="responsibility">implemented simple solution in Visual Basic 2005</div>
       </div>
     </div>
     <div class="section">
       <div class="headline">
-        <div class="company">Assistant Lecturer at Hong Kong University S.P.A.C.E. Global College</div>
-        <div class="time">2009 – 2011</div>
-      </div>
-      <div class="indent">
-        <div class="responsibility">Acquired skills in leadership and inter-cultural communication</div>
+        <span class="company">Interning Developer at Diamond Computing Company</span>
+        <span class="time">May 2012 - August 2012</span>
       </div>
     </div>
-    <h1>Education & Certification</h1>
-    <div>Certified SAFe® 6 Practitioner</div>
-    <div>Oracle Certified Professional, Java SE 6 Programmer</div>
-    <div><span class="bold">Georgia State University</span>, Atlanta, USA</div>
-    <div class="indent">Course work in Computer Science</div>
-    <div><span class="bold">Simon Frasier University</span>, Vancouver, Canada</div>
+    <div class="section">
+      <div class="headline">
+        <span class="company">Assistant Lecturer at Hong Kong University S.P.A.C.E. Global College</span>
+        <span class="time">2009 – 2011</span>
+      </div>
+    </div>
+    <h1>Education</h1>
+    <div><span class="bold">Georgia State University</span>, Atlanta, US and <span class="bold">Simon Frasier University</span>, Vancouver, Canada</div>
     <div class="indent">Course work in Computer Science</div>
     <div><span class="bold">McGill University</span>, Montreal, Canada</div>
     <div class="indent">B.A. in Linguistics, Classics</div>
     <h1>Personal</h1>
-    <div>Dual-citizen of USA and Canada</div>
+    <div>Dual-citizen of US and Canada</div>
     <div>Fluent in English, German, Conversational in Chinese, French, Spanish</div>
     <div>Summer 2015: Hiked the Appalachian Trail</div>
   </div>
@@ -198,7 +120,8 @@ a {
 
 h1 {
   padding-left: 1rem;
-
+  margin-top:.4rem;
+  margin-bottom:.3rem;
   font-size:1.6rem;
 }
 
@@ -230,9 +153,12 @@ h1 {
 }
 
 .time {
+  padding-left: .6rem;
+  font-size:1rem;
 }
 
 .company {
+  font-size:1rem;
   font-weight: bolder;
 }
 
@@ -245,15 +171,12 @@ h1 {
 }
 
 .section {
-  padding-bottom: 1rem;
+  padding-bottom: .2rem;
 }
 
 .client {
 }
 
-.project {
-  padding-bottom: .2rem;
-}
 
 .responsibility {
   display: list-item;
@@ -274,14 +197,6 @@ h1 {
 
 .bold {
   font-weight: bold;
-}
-
-.pad-x-1 {
-  padding-bottom: .5rem;
-}
-
-.pad-x-2 {
-  padding-bottom: 1rem;
 }
 .category {
   font-weight: bold;
